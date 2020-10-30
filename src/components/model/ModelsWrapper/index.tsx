2f23ceca3a1b8007f835b57aa418f386a80701d8
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-10-30 15:15:32 -0300

    ModelsWrapper component setup

--- src/components/model/ModelsWrapper/index.tsx
+++ src/components/model/ModelsWrapper/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { Container } from './styles';
 
-const ModelsWrapper: React.FC = () => {
+const ModelsWrapper: React.FC = ({children}) => {
   return (
     <Container>
-      <h1>ModelsWrapper</h1>
+      {children}
     </Container>
   );
 };
